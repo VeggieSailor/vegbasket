574b3f9afb6fee05765ac39555ca32aa15a65a27
Rafal Zawadzki <bluszcz@bluszcz.net>
2015-04-04 19:41:34 +0200

    Returning entries and regions

--- vegbasketapp/vegbasketapp/transformer/views.py
+++ vegbasketapp/vegbasketapp/transformer/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from vegbasketapp.transformer.models import Entry, Region
-from vegbasketapp.transformer.tools_entry import get_entry_by_id,get_entry_geo
+from vegbasketapp.transformer.tools_entry import get_entry_by_id
+from django.http import HttpResponse
 
 # Create your views here.
 
 def entry_map(request, entry_id):
-
 	entry = get_entry_by_id(entry_id)
 	cords = get_entry_geo(entry)
 	cords['c1lng'] = cords['lng']+0.01
@@ -13,3 +13,11 @@ def entry_map(request, entry_id):
 	cords['c1lat'] = cords['lat']+0.01
 	cords['c2lat'] = cords['lat']-0.01
 	return render(request, "entry_map.html", {'cords':cords})
+
+def entry(request, entry_id):
+	entry_tmp = get_entry_by_id(entry_id)
+	return HttpResponse(entry_tmp.results_source)
+
+def region(request, region_id):
+	region_tmp = get_entry_by_id(region_id)	
+	return HttpResponse(region_tmp.results_source)
--- vegbasketapp/vegbasketapp/urls.py
+++ vegbasketapp/vegbasketapp/urls.py
@@ -4,18 +4,12 @@ from django.conf import settings
 
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'vegbasketapp.views.home', name='home'),
     url(r'^$', 'vegbasketapp.home.views.index', name='home'),
-
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
-
     url(r'^e404$', 'vegbasketapp.home.views.handler404'),
     url(r'^e500$', 'vegbasketapp.home.views.handler500'),
-
-
+    url(r'^transformer/entry/(?P<region_id>.*)$', 'vegbasketapp.transformer.views.region', name='region'),
+    url(r'^transformer/entry/(?P<entry_id>.*)$', 'vegbasketapp.transformer.views.entry', name='entry'),
     url(r'^transformer/entry/(?P<entry_id>.*)/map$', 'vegbasketapp.transformer.views.entry_map', name='map'),
 
 
