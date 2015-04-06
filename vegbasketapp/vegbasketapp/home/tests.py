ff30543976c50cfb263244351559cb651a305cc9
bluszcz <bluszcz@bluszcz.net>
2015-04-06 20:01:04 +0200

    Unittests

--- vegbasketapp/vegbasketapp/home/tests.py
+++ vegbasketapp/vegbasketapp/home/tests.py
@@ -1,8 +1,31 @@
 from django.test import TestCase
 from django.test import Client
+from vegbasketapp.transformer.tools_entry import get_entry_by_id,get_entry_geo
 
-class HomeTestCase(TestCase):
-    def test_login_page(self):
+
+
+class EntryTestCase(TestCase):
+    def test_get_entry(self):
         c = Client()
-        r = c.get('/')
-        self.assertEqual(r.content.find('Veggie Sailor')>-1,True)
+        r = c.get('/transformer/entry/12188')
+        self.assertEqual(r.content.decode('utf-8').find('Spain')>-1,True)
+
+    def test_get_region(self):
+        c = Client()
+        r = c.get('/transformer/region/66')
+        self.assertEqual(r.content.decode('utf-8').find('Spain')>-1,True)
+
+    def test_get_map(self):
+        c = Client()
+        r = c.get('/transformer/entry/12188/map')
+        self.assertEqual(r.content.decode('utf-8').find('2.1773')>-1,True)
+
+	def test_get_reviews(self):
+        c = Client()
+        r = c.get('/transformer/entry/12188/reviews')
+        self.assertEqual(r.content.decode('utf-8').find('traditional')>-1,True)
+
+
+
+
+
--- vegbasketapp/vegbasketapp/transformer/tests.py
+++ vegbasketapp/vegbasketapp/transformer/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.test import Client
 
-# Create your tests here.
+class PicutreTestCase(TestCase):
+
+    # def test_create_user(self):
+    #     from picupwebapp.picture.models  import Picture, Gallery
+    #     from django.contrib.auth.models import User
+    #     u = User(username="test")
+    #     u.save()
+    #     self.assertEqual(u.id>0,True)
+
+    def test_home_page(self):
+        c = Client()
+        r = c.get('/')
+        self.assertEqual(r.content.decode('utf-8').find('vegan')>-1,True)
