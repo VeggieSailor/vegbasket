96aa683dd98995cf60d1101dfe89ad8e42560947
bluszcz <bluszcz@bluszcz.net>
2015-04-08 01:07:30 +0200

    #9 Actions in admin for refreshing entries and regions

--- vegbasketapp/vegbasketapp/transformer/admin.py
+++ vegbasketapp/vegbasketapp/transformer/admin.py
@@ -1,13 +1,28 @@
 from django.contrib import admin
 # Register your models here.
 from vegbasketapp.transformer.models import Entry, Region, Reviews
+from vegbasketapp.transformer.tools_entry import get_entry_by_id
+from vegbasketapp.transformer.tools_entry import get_region_by_id
+
+
+
+def refresh_entry(modeladmin, request, queryset):
+    for i in queryset.all():
+        get_entry_by_id(i.source_id, force=True)
+refresh_entry.short_description = "Refresh selected sources"
+
+def refresh_region(modeladmin, request, queryset):
+    for i in queryset.all():
+        get_region_by_id(i.source_id, force=True)
+refresh_region.short_description = "Refresh selected sources"
+
 
 class EntryAdmin(admin.ModelAdmin):
-    pass
+    actions = [refresh_entry]
 admin.site.register(Entry, EntryAdmin)
 
 class RegionAdmin(admin.ModelAdmin):
-    pass
+    actions = [refresh_region]
 admin.site.register(Region, RegionAdmin)
 
 class ReviewsAdmin(admin.ModelAdmin):
