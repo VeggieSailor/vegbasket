f0ecd7dbe92d8c42d10ff17350cd00dc90847391
Rafal Zawadzki <rafal@opportunitynetwork.com>
2015-09-28 00:31:35 +0200

    Added coverals

--- vegbasketapp/vegbasketapp/home/models.py
+++ vegbasketapp/vegbasketapp/home/models.py
@@ -17,7 +17,7 @@ class VeggieSailorEntry(models.Model):
     name = models.CharField(max_length=512, default="")
     summary = models.CharField(max_length=512, default="")
     description = models.TextField(default="")
-    region = models.ForeignKey(Region, null=False)
+    region = models.ForeignKey(VeggieSailorRegion, null=False)
     results_geo_place = models.TextField(null=False, blank=True, default="")
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
