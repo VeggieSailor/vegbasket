5fbeb0f3364858ee0b5873f148e070afc09b782b
Rafal Zawadzki <rafal@opportunitynetwork.com>
2015-09-28 00:34:56 +0200

    Fixed models

--- vegbasketapp/vegbasketapp/content/models.py
+++ vegbasketapp/vegbasketapp/content/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+import json
+
+
+class VeggieSailorRegion(models.Model):
+    name = models.CharField(max_length=512, default="")
+    parent = models.ForeignKey('self', null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)    
+    
+    
+class VeggieSailorEntry(models.Model):
+    name = models.CharField(max_length=512, default="")
+    summary = models.CharField(max_length=512, default="")
+    description = models.TextField(default="")
+    region = models.ForeignKey(VeggieSailorRegion, null=False)
+    results_geo_place = models.TextField(null=False, blank=True, default="")
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+  
