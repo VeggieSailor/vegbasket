c128fb50a072be6b8e17a581b2ede0f5543789e0
Rafal Zawadzki <rafal@opportunitynetwork.com>
2015-11-02 00:06:11 +0100

    new req

--- source/conf.py
+++ source/conf.py
@@ -20,6 +20,7 @@ import os
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('../vegbasketapp'))
+sys.path.insert(0, os.path.abspath('vegbasketapp'))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vegbasketapp.settings")
 # -- General configuration ------------------------------------------------
 
