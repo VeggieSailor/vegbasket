43edd2ac681c73fd74de491708933bceeff4a084
bluszcz <bluszcz@bluszcz.net>
2015-11-01 19:35:35 +0100

    New database fields

--- vegbasketapp/vegbasketapp/content/models.py
+++ vegbasketapp/vegbasketapp/content/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from vegbasketapp.transformer.models import Region
 
 class VeggieSailorRegion(models.Model):
-    name = models.CharField(max_length=512, default="")
+    name = models.CharField(max_length=512, default="")    
     parent = models.ForeignKey('self', null=True)
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
@@ -12,11 +12,18 @@ class VeggieSailorRegion(models.Model):
     
 class VeggieSailorEntry(models.Model):
     name = models.CharField(max_length=512, default="")
+    short_description = models.CharField(max_length=512, default="")
+    description = models.TextField(default="")    
     summary = models.CharField(max_length=512, default="")
-    description = models.TextField(default="")
     region = models.ForeignKey(VeggieSailorRegion, null=False)
     results_geo_place = models.TextField(null=False, blank=True, default="")
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
+    
 
-  
+
+class VeggieSailorCousine(models.Model):
+    name = models.CharField(max_length=128, default="")
+    parent = models.ForeignKey('self', null=True)
+    description = models.TextField(default="")
+    
