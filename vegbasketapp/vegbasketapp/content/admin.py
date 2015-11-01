a8ce3566aa8c65227aa02558b013c05f4a5f00d6
bluszcz <bluszcz@bluszcz.net>
2015-11-01 19:49:59 +0100

    Embedded map

--- vegbasketapp/vegbasketapp/content/admin.py
+++ vegbasketapp/vegbasketapp/content/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
 # Register your models here.
+from vegbasketapp.content.models import VeggieSailorRegion, VeggieSailorCousine, VeggieSailorEntry 
+
+class VSEntryAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(VeggieSailorEntry, VSEntryAdmin)
+
+class VSRegionAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(VeggieSailorRegion, VSRegionAdmin)
+
+class VSCousineAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(VeggieSailorCousine, VSCousineAdmin)
+
--- vegbasketapp/vegbasketapp/content/models.py
+++ vegbasketapp/vegbasketapp/content/models.py
@@ -3,14 +3,17 @@ from django.db import models
 from vegbasketapp.transformer.models import Region
 
 class VeggieSailorRegion(models.Model):
+    """Main Veggie Sailor Region.
+    """
     name = models.CharField(max_length=512, default="")    
     parent = models.ForeignKey('self', null=True)
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
     source_region = models.ForeignKey(Region, null=True, unique=True)
-    
-    
+
 class VeggieSailorEntry(models.Model):
+    """Main Veggie Sailor Entry.
+    """    
     name = models.CharField(max_length=512, default="")
     short_description = models.CharField(max_length=512, default="")
     description = models.TextField(default="")    
@@ -19,11 +22,12 @@ class VeggieSailorEntry(models.Model):
     results_geo_place = models.TextField(null=False, blank=True, default="")
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
-    
-
+    class Meta:
+        verbose_name_plural = "veggie sailor entries"
 
 class VeggieSailorCousine(models.Model):
+    """Main Veggie Sailor Cousine.
+    """    
     name = models.CharField(max_length=128, default="")
     parent = models.ForeignKey('self', null=True)
     description = models.TextField(default="")
-    
