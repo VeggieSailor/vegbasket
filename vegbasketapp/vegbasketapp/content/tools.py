04e57012e9582ae17ce930a50243fbc4ed79a6c1
Rafal Zawadzki <rafal@opportunitynetwork.com>
2015-11-14 00:39:23 +0100

    Some work on the converter

--- vegbasketapp/vegbasketapp/content/tools.py
+++ vegbasketapp/vegbasketapp/content/tools.py
@@ -2,7 +2,12 @@ from vegbasketapp.content.models import VeggieSailorRegion, VeggieSailorEntry
 from vegbasketapp.transformer.models import Region
 import json
 
-def convert_region(region_id):
+def get_region_id(url):
+    return url.split('/')[-1]
+
+
+
+def convert_region_down(region_id, global_list=[]):
     """Convert region.
 
     Parameters
@@ -11,8 +16,22 @@ def convert_region(region_id):
     """
     region = Region.objects.get(source_id=region_id)
     region.set_obj()
+    
+    
+    if region_id  in global_list:
+        return None
+    
+        
+    
+    #if region.obj.has_key('children'):
+        #for child in obj['children']:
+            #print child['url']
+    
+    from ipdb import set_trace; set_trace()
+    
     vs_region = VeggieSailorRegion()
     vs_region.name = region.obj['name']
     vs_region.save()
+    global_list.append(region_id)
     return (region, vs_region)
     
