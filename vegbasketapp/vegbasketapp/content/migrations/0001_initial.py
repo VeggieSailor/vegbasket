f29c7304558a250e8df540ef72d0210d80b94b46
Rafal Zawadzki <rafal@opportunitynetwork.com>
2015-11-14 20:19:05 +0100

    Order of the migrations

--- vegbasketapp/vegbasketapp/content/migrations/0001_initial.py
+++ vegbasketapp/vegbasketapp/content/migrations/0001_initial.py
@@ -6,8 +6,13 @@ from django.db import models, migrations
 
 class Migration(migrations.Migration):
 
+
     dependencies = [
+        ('contenttypes', '0001_initial'),
+        ('contenttypes', '0002_remove_content_type_name'),
     ]
+
+
 
     operations = [
         migrations.CreateModel(
