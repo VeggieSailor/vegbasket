05c8324b5a45099eec40f4c052d3dd4319c6b6dd
Rafal Zawadzki <rafal@opportunitynetwork.com>
2015-11-14 17:31:13 +0100

    Fix with Gnochi

--- vegbasketapp/vegbasketapp/content/tests.py
+++ vegbasketapp/vegbasketapp/content/tests.py
@@ -3,15 +3,15 @@ from django.test import TestCase
 from vegbasketapp.transformer.tools_entry import get_entry_by_id
 from vegbasketapp.transformer.tools_entry import get_region_by_id
 
-from vegbasketapp.content.tools import convert_region_down
+from vegbasketapp.content.tools import convert_region
 
 from vegbasketapp.content.models import VeggieSailorRegion
 
 
 class ToolsTestCase(TestCase):
     def test_convert_region_by_id(self):
-        get_region_by_id(66)
-        region, vs_region = convert_region_down(66)
+        region = get_region_by_id(66)
+        vs_region = convert_region(66)
         region.set_obj()
         self.assertEqual(region.obj['name'], 'Spain')
         self.assertEqual(region.obj['name'], vs_region.name)
