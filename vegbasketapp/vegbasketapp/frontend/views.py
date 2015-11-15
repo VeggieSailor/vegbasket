c7fbd3686c0dd4928d92b374e47cac85c881ba39
Rafal Zawadzki <rafal@opportunitynetwork.com>
2015-11-15 22:25:06 +0100

    Update new Django

--- vegbasketapp/vegbasketapp/frontend/views.py
+++ vegbasketapp/vegbasketapp/frontend/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from vegbasketapp.content.tools import get_entry_by_vg_id
+from vegbasketapp.vsdata.tools import get_entry_by_vg_id
 
 # Create your views here.
 
