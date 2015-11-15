dc9681b2b3c404e6b80d7d63d23eaefa9118593e
Rafal Zawadzki <rafal@opportunitynetwork.com>
2015-11-16 00:10:17 +0100

    New test

--- vegbasketapp/vegbasketapp/frontend/tests.py
+++ vegbasketapp/vegbasketapp/frontend/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
-
+from django.test import Client
 # Create your tests here.
+class ExampleEntry(TestCase):
+    def test_view_get_example_entry(self):
+        c = Client()
+        r = c.get('/frontend/entry_example/')
+        self.assertEqual(r.content.decode('utf-8').find('Hotel')>-1,True)
