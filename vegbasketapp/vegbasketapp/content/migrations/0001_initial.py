2c3f7512cd8b4ee9cd5f92ddc918441224e759ad
Rafal Zawadzki <rafal@opportunitynetwork.com>
2015-11-15 22:07:31 +0100

    Fixed dependency

--- vegbasketapp/vegbasketapp/content/migrations/0001_initial.py
+++ vegbasketapp/vegbasketapp/content/migrations/0001_initial.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('contenttypes', '0002_remove_content_type_name'),
+        #('contenttypes', '0002_remove_content_type_name'),
         ('transformer', '0001_initial'),
     ]
 
