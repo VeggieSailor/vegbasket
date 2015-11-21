9c4070b7d33cbc555e1263c738a9fc363250d611
Rafal Zawadzki <rafal@opportunitynetwork.com>
2015-11-21 21:50:34 +0100

    Home page with the cards

---
+++ vegbasketapp/vegbasketapp/content/templatetags/entry_cards.py
@@ -0,0 +1,11 @@
+from django import template
+from vegbasketapp.content.tools import get_entry_by_vg_id, get_vs_entry_by_id
+
+
+register = template.Library()
+
+
+@register.inclusion_tag('content/card_skel.html', takes_context=True)
+def skeleton_card_vg(context, entry_id):
+    entry = get_entry_by_vg_id(entry_id)
+    return {"entry":entry}
