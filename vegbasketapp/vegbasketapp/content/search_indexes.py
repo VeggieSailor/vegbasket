836de6f0b8b077d51c959a2fac8cd638c22b6c78
Rafal Zawadzki <rafal@opportunitynetwork.com>
2015-11-21 14:57:33 +0100

    more docs

--- vegbasketapp/vegbasketapp/content/search_indexes.py
+++ vegbasketapp/vegbasketapp/content/search_indexes.py
@@ -4,14 +4,19 @@ from haystack import indexes
 from vegbasketapp.content.models import VeggieSailorEntry
 
 class EntryIndex(indexes.SearchIndex, indexes.Indexable):
+    """Default index for the entry.
+    
+    """
     text = indexes.CharField(document=True, use_template=True)
 
     def get_model(self):
+        """Get the model.
+        
+        """
         return VeggieSailorEntry
     
     def index_queryset(self, using=None):
-        """Used when the entire index for model is updated."""
+        """Used when the entire index for model is updated.
+        
+        """
         return self.get_model().objects.all()
-    #def index_queryset(self, using=None):
-        #"""Used when the entire index for model is updated."""
-        #return self.get_model().objects.filter(pub_date__lte=datetime.datetime.now())
