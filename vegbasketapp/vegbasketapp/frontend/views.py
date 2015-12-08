f512913bcd75be0699238f2fba390ea3aafa2c49
Rafal Zawadzki <rafal@opportunitynetwork.com>
2015-12-08 18:55:05 +0100

    Add django meta - twitter cards and open graph

--- vegbasketapp/vegbasketapp/frontend/views.py
+++ vegbasketapp/vegbasketapp/frontend/views.py
@@ -30,6 +30,28 @@ def entry_vs(request, entry_id):
     """Entry by vegguide id.
     
     """
+    
+    
+    
+    
     vs_entry = get_vs_entry_by_id(entry_id)
+    
+    meta = dict(use_og=True,use_twitter=True)
+    
+    meta['title'] = vs_entry.name
+    meta['site_name'] = 'Veggie Sailor'
+    meta['description'] = vs_entry.short_description
+    meta['keywords'] = [ x.name for x in vs_entry.tags.all() ] + [ x.name for x in vs_entry.cuisines.all() ]
+    meta['object_type'] = 'restaurant.restaurant'
+    meta['twitter_site'] = '@veggiesailor'
+    meta['twitter_card'] = 'summary'
+    meta['url'] = request.build_absolute_uri(vs_entry.get_absolute_url())
+     
+    try:
+        meta['image'] = request.build_absolute_uri(vs_entry.get_images_height_400()[0].photo.url) 
+    except IndexError:
+        meta['image'] = 'https://veggiesailor.com/static/frontend/img/logo.png'
+        
+    
     return render(request, 'frontend/entry_view.html',
-                  {'entry':vs_entry})
+                  {'entry':vs_entry,'meta':meta})
--- vegbasketapp/vegbasketapp/settings.py
+++ vegbasketapp/vegbasketapp/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'meta',
     'haystack',
     'foundation',
     'vegbasketapp.home',
