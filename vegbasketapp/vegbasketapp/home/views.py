0b72f0be563eb65fd35b8daa93a0bdeedd052da6
Rafal Zawadzki <rafal@opportunitynetwork.com>
2015-12-08 20:29:44 +0100

    Cards / graph for the homepag

--- vegbasketapp/vegbasketapp/home/views.py
+++ vegbasketapp/vegbasketapp/home/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'home/index_fd.html')
+    
+    meta = dict(use_og=True,use_twitter=True)
+    meta['title'] = "Welcome to Veggie Sailor"
+    meta['site_name'] = 'Veggie Sailor'
+    meta['description'] = "Vegetarian Vegan Open Data Platform"
+    meta['keywords'] = ['vegeterian','vegan', 'bar', 'restaurant']
+    meta['object_type'] = 'article'
+    meta['twitter_site'] = '@veggiesailor'
+    meta['twitter_card'] = 'summary'
+    meta['url'] = 'https://veggiesailor.com'
+    meta['image'] = 'https://veggiesailor.com/static/frontend/img/logo.png'    
+    
+    return render(request, 'home/index_fd.html', {'meta':meta})
 
 def handler404(request):
     return render(request, '404.html')
