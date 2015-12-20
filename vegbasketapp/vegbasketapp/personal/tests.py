2d7853ef12bef839444299337604acdb04ea807d
Rafal Zawadzki <rafal@opportunitynetwork.com>
2015-12-20 16:46:05 +0100

    Fixing import inside the tests

--- vegbasketapp/vegbasketapp/personal/tests.py
+++ vegbasketapp/vegbasketapp/personal/tests.py
@@ -1,11 +1,7 @@
-from django.test import TestCase
-
-# Create your tests here.
-from django.test import TestCase
-
 import datetime
 import json
 
+from django.test import TestCase, Client
 from httpretty import HTTPretty
 
 from social.p3 import urlencode
