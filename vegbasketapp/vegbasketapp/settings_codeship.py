be63f009309d042bba263970d9d663c9b6143d98
Rafal Zawadzki <rafal@opportunitynetwork.com>
2015-12-20 17:18:27 +0100

    Settings for the codeship

---
+++ vegbasketapp/vegbasketapp/settings_codeship.py
@@ -0,0 +1,10 @@
+from vegbasketapp.settings import *
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'test',
+        'USER': os.environ.get('PG_USER'),
+        'PASSWORD': os.environ.get('PG_PASSWORD'),
+        'HOST': '127.0.0.1',
+    }
+}
