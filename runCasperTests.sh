22944fbb666734896f4bd0c42ceca7655b44b31f
Rafal Zawadzki <bluszcz@bluszcz.net>
2016-03-22 03:01:07 +0100

    Casper js files

---
+++ runCasperTests.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+casperjs screenshots.js https://veggiesailor.com/
+casperjs screenshots.js https://veggiesailor.com/en/search?q=vegan
+casperjs screenshots.js https://veggiesailor.com/en/vegan-bar-9667/
