f6babb03ad9e219df60829e7761215dca8c8722f
Rafal Zawadzki <bluszcz@bluszcz.net>
2016-03-22 03:04:06 +0100

    Casper tests

--- runCasperTests.sh
+++ runCasperTests.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-casperjs screenshots.js https://veggiesailor.com/
-casperjs screenshots.js https://veggiesailor.com/en/search?q=vegan
-casperjs screenshots.js https://veggiesailor.com/en/vegan-bar-9667/
+rm -r screenshots/*
+casperjs screenshots.js https://dev.veggiesailor.com/
+casperjs screenshots.js https://dev.veggiesailor.com/en/search?q=vegan
+casperjs screenshots.js https://dev.veggiesailor.com/en/vegan-bar-9667/
+find screenshots/  -name *png -exec  /tmp/atlassian-cli-5.1.0/hipchat.sh -t ykPWE7LXklFMlD2Td4vYZjCIj9NPfSjzbdLU1blH --action shareFile --server https://veggiesailor.hipchat.com  --room 2043965 --file '{}' \;
